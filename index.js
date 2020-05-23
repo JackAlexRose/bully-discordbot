05cda9fa25856e0697bf7974ec6cb41d09733aa4
JackAlexRose <jackalexanderrose@hotmail.com>
2020-05-23 14:23:49 +0100

    Trying wz

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ Client.on('message', async message => {
 
     API.login(`${process.env.codAccountEmail}`, `${process.env.codAccountPassword}`).then((response) => {
       //I want Warzone Data
-    API.MWfriends('PLAGUESPITTER#3141115').then(data => {
+    API.MWwz('PLAGUESPITTER#3141115').then(data => {
       console.log(data);  // see output
     }).catch(err => {
       console.log(err);
