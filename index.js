d948f7b9be786f28f4f93ccdd9c539f859aae87f
JackAlexRose <jackalexanderrose@hotmail.com>
2020-05-25 15:01:48 +0100

    Built all mp functionality

--- index.js
+++ index.js
@@ -57,8 +57,10 @@ Client.on('message', async message => {
       codStatToTrack = 'mpdeaths';
     } else if (message.content.toUpperCase().startsWith(`-COD MP STREAK`)) {
       codStatToTrack = 'mpstreak';
+    } else if (message.content.toUpperCase().startsWith(`-COD MP BESTKILLS`)) {
+      codStatToTrack = 'mpbestkills';
     } else {
-      message.channel.send("Usage is:\n-cod wz kills\n-cod wz wins\n-cod wz kd\n-cod wz deaths\n-cod mp kills\n-cod mp wins\n-cod mp kd\n-cod mp deaths\n-cod mp streak");
+      message.channel.send("Usage is:\n-cod wz kills\n-cod wz wins\n-cod wz kd\n-cod wz deaths\n-cod mp kills\n-cod mp wins\n-cod mp kd\n-cod mp deaths\n-cod mp streak\n-cod mp bestkills");
       return;
     }
 
@@ -66,14 +68,6 @@ Client.on('message', async message => {
 
     CodApi.login(`${process.env.codAccountEmail}`, `${process.env.codAccountPassword}`).then((response) => {
       var people = [];
-
-      //I want Warzone Data
-      // CodApi.MWwz(MyActivisionName).then(data => {
-      //   data.br.title = MyActivisionName.slice(0, MyActivisionName.indexOf('#'));
-      //   people.push(data.br);
-      // }).catch(err => {
-      //   console.log(err);
-      // });
 
       CodApi.MWmp(MyActivisionName).then(data => {
         data.username = data.username.slice(0, data.username.indexOf('#'));
@@ -84,23 +78,6 @@ Client.on('message', async message => {
 
       CodApi.MWfriends(MyActivisionName).then(data => {
         var counter = data.length;
-
-        // for (let key in data) {
-        //   console.log(data[key].lifetime.mode.br.properties);
-        //   usernames[key] = data[key].username;
-
-        //   CodApi.MWwz(usernames[key]).then(result => {
-        //     result.br.title = usernames[key].slice(0, usernames[key].indexOf('#'));
-        //     people.push(result.br);
-
-        //     counter--;
-        //     if (counter == 0) {
-        //       codCallback(people);
-        //     }
-        //   }).catch(err => {
-        //     console.log(err);
-        //   });
-        // }
 
         for (let key in data) {
           data[key].username = data[key].username.slice(0, data[key].username.indexOf('#'));
@@ -161,27 +138,71 @@ function codCallback(stats) {
       break;
     case "wzwins":
       codResponseMessage = "---------Warzone Wins---------\n";
-      stats.sort((a, b) => (a.wins < b.wins) ? 1 : -1);
+      stats.sort((a, b) => (a.lifetime.mode.br.properties.wins < b.lifetime.mode.br.properties.wins) ? 1 : -1);
       for (let key in stats) {
-        codResponseMessage += stats[key].title + ": " + stats[key].wins + "\n";
+        codResponseMessage += stats[key].username + ": " + stats[key].lifetime.mode.br.properties.wins + "\n";
       }
       break;
     case "wzkd":
       codResponseMessage = "---------Warzone K/D---------\n";
-      stats.sort((a, b) => (a.kdRatio < b.kdRatio) ? 1 : -1);
+      stats.sort((a, b) => (a.lifetime.mode.br.properties.kdRatio < b.lifetime.mode.br.properties.kdRatio) ? 1 : -1);
       for (let key in stats) {
         // Round the k/d to 2dp
-        stats[key].kdRatio = Math.round((stats[key].kdRatio + Number.EPSILON) * 100) / 100
-        codResponseMessage += stats[key].title + ": " + stats[key].kdRatio + "\n";
+        stats[key].lifetime.mode.br.properties.kdRatio = Math.round((stats[key].lifetime.mode.br.properties.kdRatio + Number.EPSILON) * 100) / 100
+        codResponseMessage += stats[key].username + ": " + stats[key].lifetime.mode.br.properties.kdRatio + "\n";
       }
       break;
     case "wzdeaths":
       codResponseMessage = "---------Warzone Deaths---------\n";
-      stats.sort((a, b) => (a.deaths < b.deaths) ? 1 : -1);
+      stats.sort((a, b) => (a.lifetime.mode.br.properties.deaths < b.lifetime.mode.br.properties.deaths) ? 1 : -1);
       for (let key in stats) {
-        codResponseMessage += stats[key].title + ": " + stats[key].deaths + "\n";
+        codResponseMessage += stats[key].username + ": " + stats[key].lifetime.mode.br.properties.deaths + "\n";
       }
       break;
+      case "mpkills":
+      codResponseMessage = "---------Multiplayer Kills---------\n";
+      stats.sort((a, b) => (a.lifetime.mode.all.properties.kills < b.lifetime.mode.all.properties.kills) ? 1 : -1);
+      for (let key in stats) {
+        codResponseMessage += stats[key].username + ": " + stats[key].lifetime.mode.all.properties.kills + "\n";
+      }
+      break;
+    case "mpwins":
+      codResponseMessage = "---------Multiplayer Wins---------\n";
+      stats.sort((a, b) => (a.lifetime.mode.all.properties.wins < b.lifetime.mode.all.properties.wins) ? 1 : -1);
+      for (let key in stats) {
+        codResponseMessage += stats[key].username + ": " + stats[key].lifetime.mode.all.properties.wins + "\n";
+      }
+      break;
+    case "mpkd":
+      codResponseMessage = "---------Multiplayer K/D---------\n";
+      stats.sort((a, b) => (a.lifetime.mode.all.properties.kdRatio < b.lifetime.mode.all.properties.kdRatio) ? 1 : -1);
+      for (let key in stats) {
+        // Round the k/d to 2dp
+        stats[key].lifetime.mode.all.properties.kdRatio = Math.round((stats[key].lifetime.mode.all.properties.kdRatio + Number.EPSILON) * 100) / 100
+        codResponseMessage += stats[key].username + ": " + stats[key].lifetime.mode.all.properties.kdRatio + "\n";
+      }
+      break;
+    case "mpdeaths":
+      codResponseMessage = "---------Multiplayer Deaths---------\n";
+      stats.sort((a, b) => (a.lifetime.mode.all.properties.deaths < b.lifetime.mode.all.properties.deaths) ? 1 : -1);
+      for (let key in stats) {
+        codResponseMessage += stats[key].username + ": " + stats[key].lifetime.mode.all.properties.deaths + "\n";
+      }
+      break;
+      case "mpstreak":
+        codResponseMessage = "---------Multiplayer Best Streak---------\n";
+        stats.sort((a, b) => (a.lifetime.mode.all.properties.recordKillStreak < b.lifetime.mode.all.properties.recordKillStreak) ? 1 : -1);
+        for (let key in stats) {
+          codResponseMessage += stats[key].username + ": " + stats[key].lifetime.mode.all.properties.recordKillStreak + "\n";
+        }
+        break;
+        case "mpbestkills":
+          codResponseMessage = "---------Multiplayer Best Kills In One Game---------\n";
+          stats.sort((a, b) => (a.lifetime.mode.all.properties.bestKills < b.lifetime.mode.all.properties.bestKills) ? 1 : -1);
+          for (let key in stats) {
+            codResponseMessage += stats[key].username + ": " + stats[key].lifetime.mode.all.properties.bestKills + "\n";
+          }
+          break;
     default:
       console.log("Unrecognised stat: " + codStatToTrack);
   }
